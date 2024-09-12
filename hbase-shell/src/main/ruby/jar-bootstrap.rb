a2514d288437939897bb50798040c50ecd49db19
Andrew Purtell <apurtell@salesforce.com>
2024-09-12 09:15:51 -0700

    @W-16662174 Apply HBASE-28250 Bump jruby to 9.4.8.0 to fix snakeyaml CVE; update joni and jcodings (#1225)
    * @W-16662174 Apply HBASE-28250 Bump jruby to 9.4.8.0 to fix snakeyaml CVE
    
    HBASE-28250 Bump jruby to 9.4.8.0 to fix snakeyaml CVE
    
    * Sync code as per irb 1.4.2
    * Also provide option to try irb's new functionalities for colorize and autocomplete
    
    * @W-16662174 Apply HBASE-28250 Bump jruby to 9.4.8.0 to fix snakeyaml CVE; update joni and jcondings
    
    Update joni and jcodings versions to align with the upstream branch that
    HBASE-28250 was sourced from.
    - joni: 2.1.42 -> 2.2.1
    - jcodings: 1.0.56 -> 1.0.58
    
    Add 'require jruby' to jar-bootstrap.rb from HBASE-28249 (bump jruby to 9.3.13.0 and...)

--- hbase-shell/src/main/ruby/jar-bootstrap.rb
+++ hbase-shell/src/main/ruby/jar-bootstrap.rb
@@ -37,6 +37,14 @@
 # hbase hacking.
 include Java
 
+# Required to access JRuby-specific internal features, such as `JRuby.runtime`
+# Loading 'java' was automatically loading 'jruby' until JRuby 9.2.
+# But, it has changed since JRuby 9.3. JRuby 9.3+ needs loading 'jruby' explicitly.
+#
+# See also: https://github.com/jruby/jruby/issues/7221#issuecomment-1133646241
+#
+require 'jruby'
+
 # Some goodies for hirb. Should these be left up to the user's discretion?
 if $stdin.tty?
   require 'irb/completion'
@@ -60,6 +68,8 @@ Usage: shell [OPTIONS] [SCRIPTFILE [ARGUMENTS]]
  -h | --help             This help.
  -n | --noninteractive   Do not run within an IRB session and exit with non-zero
                          status on first error.
+ -c | --colorize         Enable colorized output.
+ -a | --autocomplete     Enable auto-completion.
  --top-level-defs        Compatibility flag to export HBase shell commands onto
                          Ruby's main object
  -Dkey=value             Pass hbase-*.xml Configuration overrides. For example, to
@@ -97,6 +107,8 @@ opts = GetoptLong.new(
   ['--help', '-h', GetoptLong::NO_ARGUMENT],
   ['--debug', '-d', GetoptLong::NO_ARGUMENT],
   ['--noninteractive', '-n', GetoptLong::NO_ARGUMENT],
+  ['--colorize', '-c', GetoptLong::NO_ARGUMENT],
+  ['--autocomplete', '-a', GetoptLong::NO_ARGUMENT],
   ['--top-level-defs', GetoptLong::NO_ARGUMENT],
   ['-D', GetoptLong::REQUIRED_ARGUMENT],
   ['--return-values', '-r', GetoptLong::NO_ARGUMENT]
@@ -107,6 +119,8 @@ script2run = nil
 log_level = 'ERROR'
 @shell_debug = false
 interactive = true
+colorize = false
+autocomplete = false
 full_backtrace = false
 top_level_definitions = false
 
@@ -124,6 +138,10 @@ opts.each do |opt, arg|
     puts 'Setting DEBUG log level...'
   when '--noninteractive'
     interactive = false
+  when '--colorize'
+    colorize = true
+  when '--autocomplete'
+    autocomplete = true
   when '--return-values'
     warn '[INFO] the -r | --return-values option is ignored. we always behave '\
            'as though it was given.'
@@ -205,6 +223,8 @@ IRB.conf[:IRB_NAME] = 'hbase'
 IRB.conf[:AP_NAME] = 'hbase'
 IRB.conf[:PROMPT_MODE] = :CUSTOM
 IRB.conf[:BACK_TRACE_LIMIT] = 0 unless full_backtrace
+IRB.conf[:USE_AUTOCOMPLETE] = autocomplete
+IRB.conf[:USE_COLORIZE] = colorize
 
 # Create a workspace we'll use across sessions.
 workspace = @shell.get_workspace
